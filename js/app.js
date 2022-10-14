877e574ee71df15f346f3c43c711b382d431a855
Mohamed Mahmoud <109958214+m7bdi3@users.noreply.github.com>
2022-10-15 00:02:07 +0200

    Add active styling to section when near top of viewport, Scroll to section on link click, Add the scroll to top button Make sections collapsible Hide fixed navigation bar while not scrolling

--- js/app.js
+++ js/app.js
@@ -22,17 +22,25 @@
  * Define Global Variables
  * 
 */
-let section = document.querySelectorAll('section');
-let ul = document.querySelector('navbar_list');
-let fragment = document.createDocumentFragment();
-
+const section1 = document.querySelectorAll('section');
+const navbarList = document.getElementById('navbar__list');
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
 
-
+let buildNav = () => {
+    navbarList.innerHTML = '';
+    section1.forEach((section) => {
+        //create List item for each section 
+        let list = document.createElement('li');
+        //Edit List item's innerhtml using data-attribute 
+        list.innerHTML = `<li><a href="#${section.id}" data-nav="${section.id}" class="menu__link">${section.dataset.nav}</a></li>`;
+        // Add the list to the unOrderd list item
+        navbarList.appendChild(list);
+    });
+};
 
 /**
  * End Helper Functions
@@ -41,22 +49,79 @@ let fragment = document.createDocumentFragment();
 */
 
 // build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
+buildNav();
+// The active state styling
+let activeStyle = `background: #27565c;
+color: rgb(255, 255, 255);
+transition: ease 0.3s all;`;
+// Add active styling to section when near top of viewport
+window.onscroll = function () {
+    section1.forEach(function (active) {
+        let activeLink = navbarList.querySelector(`[data-nav=${active.id}]`);
+        if (active.getBoundingClientRect().top >= -400 && active.getBoundingClientRect().top <= 50) {
+            active.classList.add('your-active-class');
+            activeLink.style.cssText = activeStyle;
+        } else {
+            active.classList.remove('your-active-class');
+            activeLink.style.cssText = '';
+        }
+    });
+}
 /**
  * End Main Functions
  * Begin Events
  * 
 */
 
-// Build menu 
-
 // Scroll to section on link click
 
-// Set sections as active
+let anchorList = document.querySelectorAll('.menu__link');
+anchorList.forEach(link => {
+    link.onclick = function(event) {
+        event.preventDefault();
+        let destination = document.querySelector(link.hash);
+        destination.scrollIntoView({
+            behavior: 'smooth'
+        });
+        setTimeout(() => {
+            location.hash = anchorList;
+        }, 200);
+    }
+});
+
+// Add the scroll to top button
+let scrollTop = document.querySelector('.scroll-up');
+scrollTop.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+    })
+})
+
+
+//Make sections collapsible
+let coll = document.getElementsByClassName("collapsible");
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        let content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
+}
+
+//Hide fixed navigation bar while not scrolling
+const header = document.querySelector('.page__header')
+let isActive;
+document.onscroll = () => {
+    header.style.display = 'block'
+    clearTimeout(isActive)
+    isActive = setTimeout(() => {
+        header.style.display = 'none';
+    }, 2500)
+}
